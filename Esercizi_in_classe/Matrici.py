18505360a29e7d90c26df4c304b2881b9d76a5a1
Edoardo Passarino <edoardo.passarino@gmail.com>
2022-03-24 12:15:21 +0100

    Lezione 24/3/22 parte 1

--- Esercizi_in_classe/Matrici.py
+++ Esercizi_in_classe/Matrici.py
@@ -15,7 +15,7 @@ for i in range(0, n):
 
 # Esercizio 2
 
-n = int(input('Inserisci la dimensione n: '))
+'''n = int(input('Inserisci la dimensione n: '))
 M_1 = []
 for i in range(0, n):
     lista_t = []
@@ -42,4 +42,55 @@ for i in range(0, n):
 for i in range(0, n):
     for j in range(0, n):
         print(M_Prodotto[i][j], end='\t')
-    print()
+    print()'''
+
+# Esercizio 3
+
+def lettura_matrice(nome, n=None, m=None):
+    if n is None:
+       n = int(input('Inserisci la dimensione n: '))
+    if m is None:
+        m = int(input('Inserisci la dimensione m: '))
+    matrice = []
+    for i in range(0, n):
+        lista_t = []
+        for j in range(0, m):
+            lista_t.append(int(input(f'{nome} {i}:{j}: ')))
+        matrice.append(lista_t)
+    return matrice
+
+
+def prodotto_matriciale(m_1, m_2):
+    if righe(m_1) == colonne(m_2):
+        m_prodotto = []
+        for i in range(0, righe(m_1)):
+            m_prodotto.append([])
+            for j in range(0, colonne(m_2)):
+                m_prodotto[i].append(0)
+                for k in range(0, righe(m_2)):
+                    m_prodotto[i][j] += m_1[i][k]*m_2[k][j]
+        return m_prodotto
+    else:
+        print('Le righe della prima matrice devono essere uguali alle colonne della seconda')
+
+
+def stampa_matrice(m, nome=None):
+    if nome:
+        print(nome)
+    for i in range(0, righe(m)):
+        for j in range(0, colonne(m)):
+            print(m[i][j], end='\t')
+        print()
+
+
+def righe(matrice):
+    return len(matrice)
+
+
+def colonne(matrice):
+    return  len(matrice[0])
+
+
+#matrice_1 = lettura_matrice('Matrice 1'); matrice_2 = lettura_matrice('Matrice 2')
+#prodotto_1_2 = prodotto_matriciale(matrice_1,matrice_2)
+#stampa_matrice(prodotto_1_2, 'Prodotto')
